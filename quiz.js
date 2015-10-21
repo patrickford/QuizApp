bdbe68da5fbab4f0cba35de85419f8418cba2fff
Patrick Ford <patrickford@msn.com>
2015-10-20 22:40:46 -0700

    Basic question display

---
+++ quiz.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+  var quiz = {
+    name : "Presidents",
+    questions : [
+      {
+        question : "Who is the current president?",
+        answers : ["Clinton", "Obama", "Bush", "Nixon"],
+        correct : 1
+      },
+      {
+        question : "What is the President's address?",
+        answers : ["Trump Tower", "Beverly Hills 90210", "1600 Pennsylvania Ave", "Yankee Stadium"],
+        correct : 2
+      },
+      {
+        question : "How long is a presidential term?",
+        answers : ["Lifetime", "2 Years", "3 Years", "4 Years"],
+        correct : 3
+      }            
+    ]
+  };
+
+  function displayQuestion(index) {
+    var questionEl = document.getElementById("question");
+    questionEl.innerHTML = "<p>" + quiz.questions[index].question + "</p>";
+
+    var answersEl = document.getElementById("answers");
+    for (var i = 0; i < quiz.questions[index].answers.length; i++) {
+      answersEl.innerHTML += "<button>" + quiz.questions[index].answers[i] + "</button>";
+    }
+  }
+
+  var currentQuestion = 0;
+  var score = 0;
+
+  displayQuestion(currentQuestion);
+
+});
